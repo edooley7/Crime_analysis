47e83dcd19f4f1fa8de147e0b230ba99b00d0431
Erin <edooley7@gmail.com>
2015-08-13 16:40:15 -0400

    add flask app code

---
+++ app.py
@@ -0,0 +1,31 @@
+from flask import Flask
+app = flask.Flask(__name__)
+
+@app.route("/")
+def viz_page():
+    with open("error.html", 'r') as viz_file:
+        return viz_file.read()
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=80)
+
+
+
+
+
+
+"""
+import flask
+import numpy as np
+import pandas as pd
+
+
+
+app = flask.Flask(__name__)
+
+@app.route("/")
+def viz_page():
+    with open("error.html", 'r') as viz_file:
+        return viz_file.read()
+
+app.run(host='0.0.0.0', port=80)
+"""
